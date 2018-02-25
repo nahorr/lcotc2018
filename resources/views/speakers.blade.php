7e270cf6b91dbe4a26576826d38bc555f7b935b3
nahorr <nahorr@gmail.com>
2018-02-24 21:44:21 -0700

    speakers

--- resources/views/speakers.blade.php
+++ resources/views/speakers.blade.php
@@ -79,6 +79,118 @@
 			</section>
 			<!-- main-container end -->
 
+						<!-- section start -->
+			<!-- ================ -->
+			<div class="section gray-bg clearfix">
+				<div class="container">
+
+					<h1>Team - Option 3 (Gray Background)</h1>
+					<div class="separator-2"></div>
+
+					<div class="row grid-space-20">
+						<div class="col-lg-6 col-md-12">
+							<div class="image-box team-member white-bg option-3">
+								<div class="overlay-container">
+									<img src="{{asset('idea/images/team-member-1.jpg')}}" alt="">
+									<div class="overlay">
+										<ul class="social-links clearfix">
+											<li class="twitter"><a target="_blank" href="http://www.twitter.com"><i class="fa fa-twitter"></i></a></li>
+											<li class="skype"><a target="_blank" href="http://www.skype.com"><i class="fa fa-skype"></i></a></li>
+											<li class="facebook"><a target="_blank" href="http://www.facebook.com"><i class="fa fa-facebook"></i></a></li>
+										</ul>
+									</div>
+								</div>
+								<div class="image-box-body">
+									<h3 class="title">Michael Hackenbruch</h3>
+									<small>Conference Adviser</small>
+									<div class="separator-2"></div>
+									<small>Advisor Local Content / Sustainable Value Chains / Business Development Strategies</small>
+									<h3 class="title">Paper:</h3>
+									<small>Local Content and Procurement Requirements in Oil and Gas Contracts: Regional trends in the Middle East and North Africa</small>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6 col-md-12">
+							<div class="image-box team-member white-bg option-3">
+								<div class="overlay-container">
+									<img src="images/team-member-2.jpg" alt="">
+									<div class="overlay">
+										<ul class="social-links clearfix">
+											<li class="twitter"><a target="_blank" href="http://www.twitter.com"><i class="fa fa-twitter"></i></a></li>
+											<li class="skype"><a target="_blank" href="http://www.skype.com"><i class="fa fa-skype"></i></a></li>
+											<li class="facebook"><a target="_blank" href="http://www.facebook.com"><i class="fa fa-facebook"></i></a></li>
+										</ul>
+									</div>
+								</div>
+								<div class="image-box-body">
+									<h3 class="title">John Doe</h3>
+									<small>CTO</small>
+									<div class="separator-2"></div>
+									<p>Dolorum deserunt inventore iste expedita vel odio doloremque corporis officia atque enetur quam quae.</p>
+									<ul class="list-unstyled">
+										<li><i class="fa fa-phone pr-10"></i>+12 123 456 7912</li>
+										<li><i class="fa fa-mobile pr-10 pl-5"></i> +12 123 456 7912</li>
+										<li><i class="fa fa-envelope pr-10"></i><a href="mailto:johndoe@idea.com">johndoe@idea.com</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6 col-md-12">
+							<div class="image-box team-member white-bg option-3">
+								<div class="overlay-container">
+									<img src="images/team-member-3.jpg" alt="">
+									<div class="overlay">
+										<ul class="social-links clearfix">
+											<li class="twitter"><a target="_blank" href="http://www.twitter.com"><i class="fa fa-twitter"></i></a></li>
+											<li class="skype"><a target="_blank" href="http://www.skype.com"><i class="fa fa-skype"></i></a></li>
+											<li class="facebook"><a target="_blank" href="http://www.facebook.com"><i class="fa fa-facebook"></i></a></li>
+										</ul>
+									</div>
+								</div>
+								<div class="image-box-body">
+									<h3 class="title">Anne Smith</h3>
+									<small>project Manager</small>
+									<div class="separator-2"></div>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, optio voluptate sapiente hic.</p>
+									<ul class="list-unstyled">
+										<li><i class="fa fa-phone pr-10"></i>+12 123 456 7912</li>
+										<li><i class="fa fa-mobile pr-10 pl-5"></i>+12 123 456 7912</li>
+										<li><i class="fa fa-envelope pr-10"></i><a href="mailto:annesmith@idea.com">annesmith@idea.com</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6 col-md-12">
+							<div class="image-box team-member white-bg option-3">
+								<div class="overlay-container">
+									<img src="images/team-member-4.jpg" alt="">
+									<div class="overlay">
+										<ul class="social-links clearfix">
+											<li class="twitter"><a target="_blank" href="http://www.twitter.com"><i class="fa fa-twitter"></i></a></li>
+											<li class="skype"><a target="_blank" href="http://www.skype.com"><i class="fa fa-skype"></i></a></li>
+											<li class="facebook"><a target="_blank" href="http://www.facebook.com"><i class="fa fa-facebook"></i></a></li>
+										</ul>
+									</div>
+								</div>
+								<div class="image-box-body">
+									<h3 class="title">Margaret Manson</h3>
+									<small>Developer</small>
+									<div class="separator-2"></div>
+									<p>Dolorum deserunt inventore iste expedita vel odio doloremque corporis officia atque enetur quam quae.</p>
+									<ul class="list-unstyled">
+										<li><i class="fa fa-phone pr-10"></i>+12 123 456 7912</li>
+										<li><i class="fa fa-mobile pr-10 pl-5"></i> +12 123 456 7912</li>
+										<li><i class="fa fa-envelope pr-10"></i><a href="mailto:margaretmanson@idea.com">margaretmanson@idea.com</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="space"></div>
+				</div>
+			</div>
+			<!-- section end -->
+
         @include('includes.footer')
 
         @include('includes.subfooter')
