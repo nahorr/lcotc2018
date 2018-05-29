62227a432e62b2768dcd3c94cdae72e734d94cc1
Nnamdi Okeke <nahorr@gmail.com>
2018-05-29 15:21:48 -0600

    pr

--- resources/views/press-release.blade.php
+++ resources/views/press-release.blade.php
@@ -4,19 +4,6 @@
 
             @include('includes.header')
 
-            <!-- banner start -->
-			<!-- ================ -->
-			<div class="banner">
-				<div class="fixed-image section light-translucent-bg" style="background-image:url('idea/images/press-release.jpg');">
-					<div class="container">
-					<div class="space-top"></div>
-					<h1>Pictures-2018 Conference</h1>
-					<div class="separator-2"></div>
-					<p class="lead">The Local Content Conference is being organised by Marak Alliance LLC in conjunction with international local content experts.<br /> Contact: info@marakconsulting.com</p>
-					</div>
-				</div>
-			</div>
-			<!-- banner end -->
 
 			<!-- main-container start -->
 
@@ -29,23 +16,13 @@
 			  <div class="container">
 
 			    <div class="row">
-			    	<div class="row">
-				    	<div class="col-md-6">
-				    		<img src="{{ asset('press_release/pr-img1.jpg') }}" class="img-responsive" alt="press-release" width="100%" height="300">
-				    	</div>
-				    	<div class="col-md-6">
-				    		<img src="{{ asset('press_release/pr-img2.jpg') }}" class="img-responsive" alt="press-release" width="100%" height="300">
-				    	</div>
+			    	<div class="col-md-10 col-md-offset-1">
+			    		<img src="{{ asset('press_release/1.png') }}" class="img-responsive" alt="press-release" width="100%" height="300">
+			    		<img src="{{ asset('press_release/2.png') }}" class="img-responsive" alt="press-release" width="100%" height="300">
+			    		<img src="{{ asset('press_release/3.png') }}" class="img-responsive" alt="press-release" width="100%" height="300">
+			    		<img src="{{ asset('press_release/4.png') }}" class="img-responsive" alt="press-release" width="100%" height="300">
 				    </div>
-			    	<hr>
-			    	<div class="row">
-			    		<iframe src="{{ asset('press_release/pr-pdf-1.pdf') }}" width="100%" height="1200"></iframe>
-			    	</div>
-			    	<hr>
-			    	<div class="row">
-			    		<iframe src="{{ asset('press_release/pr-doc-1.pdf') }}" width="100%" height="1200"></iframe>
-			    	</div>
-
+			    
 
 			    </div>
 
@@ -56,8 +33,7 @@
 			<!-- main-container end -->
 
 
-        @include('includes.footer')
-
+       
         @include('includes.subfooter')
 @endsection
 
