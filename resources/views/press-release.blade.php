3904c9d28c0a55a179a0c292991024a2e8a4839a
Nnamdi Okeke <nahorr@gmail.com>
2018-05-30 09:10:13 -0600

    press release

--- resources/views/press-release.blade.php
+++ resources/views/press-release.blade.php
@@ -17,10 +17,10 @@
 
 			    <div class="row">
 			    	<div class="col-md-10 col-md-offset-1">
-			    		<img src="{{ asset('press_release/1.png') }}" class="img-responsive" alt="press-release" width="100%" height="300">
-			    		<img src="{{ asset('press_release/2.png') }}" class="img-responsive" alt="press-release" width="100%" height="300">
-			    		<img src="{{ asset('press_release/3.png') }}" class="img-responsive" alt="press-release" width="100%" height="300">
-			    		<img src="{{ asset('press_release/4.png') }}" class="img-responsive" alt="press-release" width="100%" height="300">
+			    		<img src="{{ asset('press_release/1.jpg') }}" class="img-responsive" alt="press-release" width="100%" height="auto">
+			    		<img src="{{ asset('press_release/2.jpg') }}" class="img-responsive" alt="press-release" width="100%" height="auto">
+			    		<img src="{{ asset('press_release/3.jpg') }}" class="img-responsive" alt="press-release" width="100%" height="auto">
+			    		<img src="{{ asset('press_release/4.png') }}" class="img-responsive" alt="press-release" width="100%" height="auto">
 				    </div>
 			    
 
